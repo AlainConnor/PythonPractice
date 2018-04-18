1a0d24908140254d48b5fa5d540f2d6b173d8365
anlei <727941483@qq.com>
2018-04-18 13:40:57 +0800

    更改为py3版本

--- imooc_project/3_4.py
+++ imooc_project/3_4.py
@@ -1,6 +1,3 @@
-
-
-
 class FloatRange:
     def __init__(self, start, end, step=0.1):
         self.start = start
@@ -19,5 +16,7 @@ class FloatRange:
             yield t
             t -= self.step
 
+
 for x in reversed(FloatRange(1.0, 4.0, 0.5)):
-    print x
+    print(x)
+
