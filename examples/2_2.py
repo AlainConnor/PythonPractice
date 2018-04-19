c23c5ab073979a09a900492e05266db488de33d3
anlei <727941483@qq.com>
2018-04-19 14:04:35 +0800

    '完成2_2的example'

--- examples/2_2.py
+++ examples/2_2.py
@@ -1,22 +1,29 @@
-from collections import namedtuple
 # 如何为元组中的每个元素命名, 提高程序可读性
+from collections import namedtuple
+
 NAME = 0
 AGE = 1
 SEX = 2
-NAME, AGE, SEX, EMAIL = range(4)
+# 可替换的更为简单的语法
+# NAME, AGE, SEX, EMAIL = range(4)
 student = ('Jim', 16, 'male', 'jim@gmail.com')
+
 # name
-print(student[NAME])
+print("name:", student[NAME])
 # age
-print(student[AGE])
+print("age:", student[AGE])
 # sex
-print(student[SEX])
+print("sex:", student[SEX])
 
+print("--------------------------------------------------------------")
+print("使用namedtuple对象")
+# 实例化namedtuple对象
 student = namedtuple('Student', ['name', 'age', 'sex', 'email'])
-s1 = student('Jim', 16, 'male', 'jim@gmail.com')
+s1 = student('Jim', 16, 'male', 'jim@gmail.com')  # 使用student来创建一个元组,按顺序传值
 print(s1)
-s2 = student(name='Bob', age=20, sex='female', email='bob@gmail.com')
-print(s2.name)
-print(s2.age)
+s2 = student(name='Bob', age=20, sex='female', email='bob@gmail.com')  # 关键字传值
+print("属性方式访问：", s2.name, s2.age)
+print("数组关键字索引访问：", s2.name, s2.age)
 
+# 判断s1是否为tuple的子类
 print(isinstance(s1, tuple))
